990ef3e3cfdeb66bb78b6a1d5ae6d5848a28dacf
djolemilic <djolemilic51@gmail.com>
2023-03-13 21:23:35 +0100

    v1.2

--- EsDnevnik/EsDnevnik/EsDnevnik/Raspodela.Designer.cs
+++ EsDnevnik/EsDnevnik/EsDnevnik/Raspodela.Designer.cs
@@ -29,14 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Obrisi = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Menjaj = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Dodaj = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Nastavnik = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.Godina = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.Predmet = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.Odeljenje = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -47,6 +39,14 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nastavnik = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Godina = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Predmet = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Odeljenje = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Obrisi = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Menjaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Dodaj = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,14 +54,14 @@
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Obrisi,
-            this.Menjaj,
-            this.Dodaj,
             this.id,
             this.Nastavnik,
             this.Godina,
             this.Predmet,
-            this.Odeljenje});
+            this.Odeljenje,
+            this.Obrisi,
+            this.Menjaj,
+            this.Dodaj});
             this.dataGridView1.Location = new System.Drawing.Point(21, 74);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
@@ -69,34 +69,96 @@
             this.dataGridView1.Size = new System.Drawing.Size(1279, 490);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            //this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
             // 
-            // Obrisi
+            // textBox1
             // 
-            this.Obrisi.HeaderText = "Obrisi";
-            this.Obrisi.MinimumWidth = 6;
-            this.Obrisi.Name = "Obrisi";
-            this.Obrisi.Text = "Obrisi";
-            this.Obrisi.UseColumnTextForButtonValue = true;
-            this.Obrisi.Width = 125;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(21, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 22);
+            this.textBox1.TabIndex = 1;
             // 
-            // Menjaj
+            // textBox2
             // 
-            this.Menjaj.HeaderText = "Menjaj";
-            this.Menjaj.MinimumWidth = 6;
-            this.Menjaj.Name = "Menjaj";
-            this.Menjaj.Text = "Menjaj";
-            this.Menjaj.UseColumnTextForButtonValue = true;
-            this.Menjaj.Width = 125;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(257, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 22);
+            this.textBox2.TabIndex = 2;
             // 
-            // Dodaj
+            // textBox3
             // 
-            this.Dodaj.HeaderText = "Dodaj";
-            this.Dodaj.MinimumWidth = 6;
-            this.Dodaj.Name = "Dodaj";
-            this.Dodaj.Text = "Dodaj";
-            this.Dodaj.UseColumnTextForButtonValue = true;
-            this.Dodaj.Width = 125;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(493, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(230, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(726, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(230, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(965, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(230, 22);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(21, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(254, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nastavnik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(490, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Godina";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Location = new System.Drawing.Point(726, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Predmet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(962, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Odeljenje";
             // 
             // id
             // 
@@ -134,95 +196,32 @@
             this.Odeljenje.Name = "Odeljenje";
             this.Odeljenje.Width = 125;
             // 
-            // textBox1
+            // Obrisi
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(21, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 22);
-            this.textBox1.TabIndex = 1;
+            this.Obrisi.HeaderText = "Obrisi";
+            this.Obrisi.MinimumWidth = 6;
+            this.Obrisi.Name = "Obrisi";
+            this.Obrisi.Text = "Obrisi";
+            this.Obrisi.UseColumnTextForButtonValue = true;
+            this.Obrisi.Width = 125;
             // 
-            // textBox2
+            // Menjaj
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(257, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(230, 22);
-            this.textBox2.TabIndex = 2;
+            this.Menjaj.HeaderText = "Menjaj";
+            this.Menjaj.MinimumWidth = 6;
+            this.Menjaj.Name = "Menjaj";
+            this.Menjaj.Text = "Menjaj";
+            this.Menjaj.UseColumnTextForButtonValue = true;
+            this.Menjaj.Width = 125;
             // 
-            // textBox3
+            // Dodaj
             // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(493, 27);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(230, 22);
-            this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(729, 27);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(230, 22);
-            this.textBox4.TabIndex = 4;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(965, 27);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(230, 22);
-            this.textBox5.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Enabled = false;
-            this.label1.Location = new System.Drawing.Point(21, 5);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 16);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Id";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Enabled = false;
-            this.label2.Location = new System.Drawing.Point(254, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 16);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Nastavnik";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Enabled = false;
-            this.label3.Location = new System.Drawing.Point(490, 8);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 16);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Godina";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Enabled = false;
-            this.label4.Location = new System.Drawing.Point(726, 8);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 16);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Predmet";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Enabled = false;
-            this.label5.Location = new System.Drawing.Point(962, 8);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 16);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Odeljenje";
+            this.Dodaj.HeaderText = "Dodaj";
+            this.Dodaj.MinimumWidth = 6;
+            this.Dodaj.Name = "Dodaj";
+            this.Dodaj.Text = "Dodaj";
+            this.Dodaj.UseColumnTextForButtonValue = true;
+            this.Dodaj.Width = 125;
             // 
             // Raspodela
             // 
@@ -252,14 +251,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewButtonColumn Obrisi;
-        private System.Windows.Forms.DataGridViewButtonColumn Menjaj;
-        private System.Windows.Forms.DataGridViewButtonColumn Dodaj;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Nastavnik;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Godina;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Predmet;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Odeljenje;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
@@ -270,5 +261,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Nastavnik;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Godina;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Predmet;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Odeljenje;
+        private System.Windows.Forms.DataGridViewButtonColumn Obrisi;
+        private System.Windows.Forms.DataGridViewButtonColumn Menjaj;
+        private System.Windows.Forms.DataGridViewButtonColumn Dodaj;
     }
 }
